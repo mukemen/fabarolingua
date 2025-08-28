1828e6f5ffcf98d0310fc82426f931ce99230e09
FAJAR MUKEMEN <43581831+mukemen@users.noreply.github.com>
2025-08-28 16:53:27 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,12 @@
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('fabaro-lingua-v1').then((cache) => cache.addAll([
-      '/',
-      '/manifest.json',
-      '/icons/icon-192.png',
-      '/icons/icon-512.png',
-    ]))
+    caches.open("fabaro-lingua-v1").then((cache) =>
+      cache.addAll(["/", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"])
+    )
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
