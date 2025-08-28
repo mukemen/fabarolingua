4078dca56ceaa0f62bd205b0e63ef3f80f5f29f4
FAJAR MUKEMEN <43581831+mukemen@users.noreply.github.com>
2025-08-28 16:51:09 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { optimizePackageImports: [] },
-  images: { unoptimized: true }, // keep simple for Vercel free tier & PWA
+  images: { unoptimized: true }
 };
-
 module.exports = nextConfig;
