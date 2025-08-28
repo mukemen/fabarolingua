c5092fd28d92696fa1f924391132dc2b80a45d0f
FAJAR MUKEMEN <43581831+mukemen@users.noreply.github.com>
2025-08-28 16:13:18 +0700

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('fabaro-lingua-v1').then((cache) => cache.addAll([
+      '/',
+      '/manifest.json',
+      '/icons/icon-192.png',
+      '/icons/icon-512.png',
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
